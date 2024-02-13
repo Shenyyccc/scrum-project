a0825d6cdbb4bca99a8e37ffd3f43b67d593f771
shenyc <112206047@qq.com>
2024-02-13 10:16:37 +0800

    #font-end:element-UI整合

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Main from '../views/Main.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode:"history",
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Main',
+      component: Main
     }
   ]
 })
