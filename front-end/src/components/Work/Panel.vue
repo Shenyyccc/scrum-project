d6bc4c8a773a4556985dc2e2b750f41f667e6671
shenyc <112206047@qq.com>
2024-02-18 20:18:52 +0800

    #draggable+bpmn.js整合

--- front-end/src/components/Work/Panel.vue
+++ front-end/src/components/Work/Panel.vue
@@ -3,42 +3,39 @@
     <h1>{{drag?'拖拽中':'拖拽停止'}}</h1>
     <div class="container">
       <!--    <div>{{drag?'拖拽中':'拖拽停止'}}</div>-->
-      <div>
+      <div class="draggable-container">
         <h1>未开始</h1>
         <!--使用draggable组件-->
-        <draggable v-model="myArray"  chosenClass="chosen" forceFallback="true" group="people"
-                   animation="300" @start="onStart" @end="onEnd">
-          <transition-group>
-            <div class="item" v-for="element in myArray" :key="element.id">{{element.name}}</div>
-          </transition-group>
+        <draggable id="1" v-model="myArray"  chosenClass="chosen" forceFallback="true" group="people"
+                   animation="300" @start="onStart" @end="onEnd"  style="overflow-y: scroll;height: 450px">
+<!--          <transition-group>-->
+            <div class="item" v-for="element in myArray" :key="element.id" style="margin:10px">{{element.name}}</div>
+<!--          </transition-group>-->
         </draggable>
       </div>
 
-      <div>
+      <div class="draggable-container" >
         <h1>进行中</h1>
-        <draggable v-model="myArray1"  chosenClass="chosen" forceFallback="true" group="people"
-                   animation="300" @start="onStart" @end="onEnd">
-          <transition-group>
-            <div class="item" v-for="element in myArray1" :key="element.id">{{element.name}}</div>
-          </transition-group>
+        <draggable id='2' v-model="myArray1"  chosenClass="chosen" forceFallback="true" group="people"
+                   animation="300" @start="onStart" @end="onEnd" >
+<!--          <transition-group>-->
+            <div class="item" v-for="element in myArray1" :key="element.id" style="margin: 10px">{{element.name}}</div>
+<!--          </transition-group>-->
         </draggable>
       </div>
 
-      <div>
+      <div class="draggable-container">
         <h1>已完成</h1>
-        <draggable v-model="myArray2"  chosenClass="chosen" forceFallback="true" group="people"
-                   animation="300" @start="onStart" @end="onEnd">
-          <transition-group>
-            <div class="item" v-for="element in myArray2" :key="element.id">{{element.name}}</div>
-          </transition-group>
+        <draggable id='3' v-model="myArray2"  chosenClass="chosen" forceFallback="true" group="people"
+                   animation="300" @start="onStart" @end="onEnd" >
+<!--          <transition-group>-->
+            <div class="item" v-for="element in myArray2" :key="element.id" style="margin:10px">{{element.name}}</div>
+<!--          </transition-group>-->
         </draggable>
       </div>
     </div>
 
   </div>
-
-
-
 
 
 </template>
@@ -62,16 +59,16 @@ export default {
         {people:'us',id:4,name:'www.google.com'}
       ],
       myArray1:[
-        {people:'1',id:1,name:'www.itxst.com'},
-        {people:'2',id:2,name:'www.baidu.com'},
-        {people:'3',id:3,name:'www.taobao.com'},
-        {people:'4',id:4,name:'www.google.com'}
+        {people:'1',id:5,name:'www.itxst.com'},
+        {people:'2',id:6,name:'www.baidu.com'},
+        {people:'3',id:7,name:'www.taobao.com'},
+        {people:'4',id:8,name:'www.google.com'}
       ],
       myArray2:[
-        {people:'1',id:1,name:'www.itxst.com'},
-        {people:'2',id:2,name:'www.baidu.com'},
-        {people:'3',id:3,name:'www.taobao.com'},
-        {people:'4',id:4,name:'www.google.com'}
+        {people:'1',id:9,name:'www.itxst.com'},
+        {people:'2',id:10,name:'www.baidu.com'},
+        {people:'3',id:11,name:'www.taobao.com'},
+        {people:'4',id:12,name:'www.google.com'}
       ]
     };
   },
@@ -81,8 +78,20 @@ export default {
       this.drag=true;
     },
     //拖拽结束事件
-    onEnd() {
+    onEnd(evt) {
       this.drag=false;
+      const draggedItem = evt.item._underlying_vm_;
+      const oldIndex = evt.oldIndex
+      const newIndex = evt.newIndex;
+      const fromList = evt.from.getAttribute('id');
+      const toList = evt.to.getAttribute('id');
+
+      console.log(evt.relatedContext)
+
+      console.log(`Old index: ${oldIndex}`);
+      console.log(`New index: ${newIndex}`);
+      console.log(`From list: ${fromList}`);
+      console.log(`To list: ${toList}`);
     },
   },
 };
@@ -107,5 +116,16 @@ export default {
 .chosen {
   border: solid 2px #3089dc !important;
   color: red;
+}
+
+.draggable-container{
+  width: 400px;
+  height: 500px;
+  margin: 30px;
+  background: #a4a4a5
+}
+
+transition div{
+  margin: 20px;
 }
 </style>
--- front-end/src/components/Work/WorkList.vue
+++ front-end/src/components/Work/WorkList.vue
@@ -1,13 +1,97 @@
 <template>
-<h1>workList</h1>
+  <div class="containers">
+    <div class="canvas" ref="canvas"></div>
+    <div id="js-properties-panel" class="panel"></div>
+  </div>
 </template>
 
 <script>
+/* bpmn-js 主体依赖 */
+import BpmnModeler from 'bpmn-js/lib/Modeler' // Bpmn-Modeler
+/* bpmn-js 左侧工具栏 */
+import 'bpmn-js/dist/assets/diagram-js.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css'
+/* bpmn-js 右侧属性栏 */
+import 'bpmn-js-properties-panel/dist/assets/bpmn-js-properties-panel.css'
+import propertiesPanelModule from 'bpmn-js-properties-panel'
+import propertiesProviderModule from 'bpmn-js-properties-panel/lib/provider/camunda'
+/* 要渲染的bpmn文件内容（实质为xml） */
+import xml from '@/plugins/defaultXml' // 样例直接预置xml，实际应该从后台获得
+
 export default {
-  name: "WorkList"
+  name: '',
+  components: {},
+  // 生命周期 - 创建完成（可以访问当前this实例）
+  created() {
+  },
+  // 生命周期 - 载入后, Vue 实例挂载到实际的 DOM 操作完成，一般在该过程进行 Ajax 交互
+  mounted() {
+    this.init()
+  },
+  data() {
+    return {
+      // bpmn建模器
+      bpmnModeler: null,
+      container: null,
+      canvas: null
+    }
+  },
+  methods: {
+    init() {
+      // 获取到属性ref为“canvas”的dom节点
+      const canvas = this.$refs.canvas
+      // 建模
+      this.bpmnModeler = new BpmnModeler({
+        // 基础画布
+        container: canvas,
+        // 控制板
+        propertiesPanel: {
+          parent: '#js-properties-panel'
+        },
+        // 插件
+        additionalModules: [
+          propertiesPanelModule,
+          propertiesProviderModule,  // 右侧属性栏
+        ],
+      })
+      this.createNewDiagram()
+    },
+    createNewDiagram() {
+      console.log('xml', xml)
+      // 将字符串转换成图显示出来
+      this.bpmnModeler.importXML(xml, (err) => {
+        if (err) {
+          // console.error(err)
+        } else {
+          // 这里是成功之后的回调, 可以在这里做一系列事情
+          this.success()
+        }
+      })
+    },
+    success() {
+      // console.log('创建成功!')
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.containers{
+  position: absolute;
+  background-color: #ffffff;
+  width: 80%;
+  height: 100%;
+}
+.canvas{
+  width: 100%;
+  height: 100%;
+}
+.panel{
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 300px;
+}
 </style>
